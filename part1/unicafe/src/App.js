06b5e92943566c4c36313251ea2b42c6abcc4773
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-10-21 14:14:38 +0300

    1.6: unicafe step1(fix header)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,34 +1,7 @@
 import {useState} from "react";
 
-/*
-const Display = (props) => <div>{props.value}</div>
-
-const Button = (props) => (
-    <button onClick={props.handleClick}>
-        {props.text}
-    </button>
-)
-
-const App = () => {
-    const [value, setValue] = useState(10)
-
-    const setToValue = (newValue) => {
-        console.log('value now', newValue)
-        setValue(newValue)
-    }
-
-    return (
-        <div>
-            <Display value={value} />
-            <Button handleClick={() => setToValue(1000)} text='thousand'/>
-            <Button handleClick={() => setToValue(0)} text='reset'/>
-            <Button handleClick={() => setToValue(value + 1)} text='increment'/>
-        </div>
-    )
-}
-*/
 const Display = (props) => <div>{props.text}{props.feedback}</div>
-const Header = (props) => <div>{props.text}</div>
+const Header = (props) => <h2>{props.text}</h2>
 const Button = (props) => (
     <button onClick={props.handleClick}>
         {props.text}
@@ -57,14 +30,13 @@ const App = () => {
     return (
         <div>
             <Header text='give feedback'/>
-            <br/>
+
             <Button handleClick={() => setToGood(good + 1)} text='good'/>
             <Button handleClick={() => setToNeutral( neutral + 1)} text='neutral'/>
             <Button handleClick={() => setToBad(bad + 1)} text='bad'/>
-            <br/>
-            <br/>
+
             <Header text='statistics'/>
-            <br/>
+
             <Display text='good ' feedback={good} />
             <Display text='neutral ' feedback={neutral} />
             <Display text='bad ' feedback={bad} />
