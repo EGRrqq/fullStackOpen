2eb84f4641dd6ffafd4381cd74833b1945e533c3
EGRrqq <kakandro1337@gmail.com>
2022-10-29 03:35:17 +0300

    2.12* step1
    Display and filter the list of countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Filter = (props) => {
+    return (
+        <div>
+            find countries: <input value={props.value} onChange={props.onChange}/>
+        </div>
+    )
+}
+
+/*const ShowCountry = (props) => {
+    return (
+        <div>
+            <li key={props.index}>{props.country}</li>
+        </div>
+    )
+}*/
+
+const ShowCountries = (props) => {
+    return (
+        <ul>
+            {props.countries.map((country, index) =>
+                <li key={index}>{country.name.common}</li>
+            )}
+        </ul>
+    )
+}
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        console.log('promise fulfilled')
+        axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(response => {
+                setCountries(response.data)
+            })
+    }, [])
+
+    const countriesToShow = () => {
+        const regExp = new RegExp(search, 'i')
+        return countries.filter( country => country.name.common.match(regExp))
+    }
+
+    const handleCountriesShow = (event) => {
+        setSearch(event.target.value)
+    }
+
+    return (
+        <div>
+            <Filter value={search} onChange={handleCountriesShow} />
+            <ShowCountries countries={countriesToShow()} />
+        </div>
+    )
 }
 
 export default App;
