542cfa652f36a5a3f791b89d6a53d5f0cb53572c
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-11-22 17:30:58 +0300

    4.20*: bloglist expansion, step8

--- part4/app.js
+++ part4/app.js
@@ -30,6 +30,8 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
+app.use(middleware.tokenExtractor)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
