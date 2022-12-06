83ce445d9a93fcc9af9cb14850f5c0a212e2c193
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-12-06 16:29:56 +0300

    5.9: Blog list frontend, step9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -126,9 +126,11 @@ const App = () => {
                 <BlogForm createBlog={addBlog} />
             </Togglable>
 
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} updateBlog={handleLike}/>
-            )}
+            {blogs
+                .sort((a, b) => b.likes - a.likes)
+                .map(blog =>
+                    <Blog key={blog.id} blog={blog} updateBlog={handleLike}/>
+                )}
         </div>
     )
 }
