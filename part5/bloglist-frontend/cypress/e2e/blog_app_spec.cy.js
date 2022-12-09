154b6c9edf1f2fb5cab1e2a3edc0611b22d455ee
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-12-09 18:46:55 +0300

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app_spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app_spec.cy.js
@@ -1,6 +1,14 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = {
+      username: 'ilovejerseyclub',
+      name: 'eeeeee',
+      password: 'ilovejerseyclub'
+    }
+
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +17,23 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.contains('button', 'login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('ilovejerseyclub')
+      cy.get('#password').type('ilovejerseyclub')
+      
+      cy.contains('button', 'login').click()
+      cy.contains('ilovejerseyclub logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('wrong')
+      cy.get('#password').type('wrong')
+
+      cy.contains('button', 'login').click()
+      cy.contains('invalid username or password')
+    })
   })
 })
