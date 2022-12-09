fcb95e58054f9442794184a8c3d617d0090b6e06
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-12-09 20:16:15 +0300

    5.20 - 5.21: bloglist end to end testing, step5

--- part5/bloglist-frontend/cypress/e2e/blog_app_spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app_spec.cy.js
@@ -8,7 +8,14 @@ describe('Blog app', function() {
       password: 'ilovejerseyclub'
     }
 
+    const anotherUser = {
+      username: 'ilovejerseyclubtoo',
+      name: 'qqqqqq',
+      password: 'ilovejerseyclubtoo'
+    }
+
     cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.request('POST', 'http://localhost:3003/api/users/', anotherUser)
     cy.visit('http://localhost:3000')
   })
 
@@ -53,6 +60,33 @@ describe('Blog app', function() {
 
       cy.contains('button', 'create').click()
       cy.contains('a new blog ilovejerseyclub by e added')
+    })
+
+    describe('and one blog is added', function() {
+      beforeEach(function() {
+        cy.createBlog({ title: 'ilovejerseyclubsomuch', author: 'e', url: 'https://soundcloud.com/' })
+      })
+
+      it('a blog can be liked', function() {
+        cy.contains('button', 'show').click()
+        cy.contains('button', 'like').click()
+        cy.contains('likes: 1')
+      })
+
+      it('a blog can be deleted', function() {
+        cy.contains('button', 'show').click()
+        cy.contains('button', 'remove').click()
+        cy.get('html')
+            .should('not.contain', 'ilovejerseyclubsomuch')
+      })
+
+      it('other user cannot delete the blog', function () {
+        cy.login({ username: 'ilovejerseyclubtoo', password: 'ilovejerseyclubtoo' })
+
+        cy.contains('button', 'show').click()
+        cy.contains('button', 'remove').click()
+        cy.contains('only the author of the blog can delete it')
+      })
     })
   })
 })
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -32,15 +32,15 @@ const Blog = ({ blog, updateBlog, deleteBlog }) => {
     return (
         <div style={blogStyle} className='blog-container'>
             {blog.title} {blog.author}
-            <button onClick={toggleDetails} className='details-button'>{visible ? 'hide' : 'show'}</button>
+            <button onClick={toggleDetails} id='details-button'>{visible ? 'hide' : 'show'}</button>
             <div style={hideDetails} className='blog-details'>
                 <div>{blog.url}</div>
                 <div>
                     likes: {blog.likes}
-                    <button onClick={incrementLikes} className='like-button'>like</button>
+                    <button onClick={incrementLikes} id='like-button'>like</button>
                 </div>
                 <div>{blog.user.name}</div>
-                <button onClick={removeBlog} className='remove-button'>remove</button>
+                <button onClick={removeBlog} id='remove-button'>remove</button>
             </div>
         </div>
     )
