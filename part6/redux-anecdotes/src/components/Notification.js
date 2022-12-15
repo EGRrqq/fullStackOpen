fc85f834ae66792e38e3679c39f6f3ef5aead499
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-12-15 15:28:50 +0300

    6.19 anecdotes and connect, step1
    To finish this part we will look into another older and more complicated way to use redux, the connect-function provided by react-redux.
    
    In new applications you should absolutely use the hook-api, but knowing how to use connect is useful when maintaining older projects using redux.

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,7 +1,8 @@
-import { useSelector } from 'react-redux'
+/*import { useSelector } from 'react-redux'*/
+import { connect } from "react-redux"
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+/*  const notification = useSelector(state => state.notification)*/
   const style = {
     border: 'solid',
     padding: 10,
@@ -10,12 +11,21 @@ const Notification = () => {
 
   return (
       <>
-        {notification &&
+        {props.notification &&
             <div style={style}>
-              {notification}
+              {props.notification}
             </div>}
       </>
   )
 }
 
-export default Notification
+/*
+export default Notification*/
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotifications = connect(mapStateToProps)(Notification)
+export default ConnectedNotifications
