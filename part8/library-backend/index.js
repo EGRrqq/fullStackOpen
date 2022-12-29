b0a36289584da27b95a474f06c4e7103ad82f28d
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2022-12-29 15:53:50 +0300

    8.13 - 8.14: Database, part 2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,91 +1,33 @@
-const { ApolloServer, gql } = require('apollo-server')
-const { v1: uuid } = require('uuid')
+const {
+    ApolloServer,
+    gql,
+} = require('apollo-server')
 
-let authors = [
-    {
-        name: 'Robert Martin',
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952,
-    },
-    {
-        name: 'Martin Fowler',
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963
-    },
-    {
-        name: 'Fyodor Dostoevsky',
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821
-    },
-    {
-        name: 'Joshua Kerievsky', // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: 'Sandi Metz', // birthyear not known
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-]
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Book = require('./models/book')
+const Author = require('./models/author')
 
-let books = [
-    {
-        title: 'Clean Code',
-        published: 2008,
-        author: 'Robert Martin',
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Agile software development',
-        published: 2002,
-        author: 'Robert Martin',
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ['agile', 'patterns', 'design']
-    },
-    {
-        title: 'Refactoring, edition 2',
-        published: 2018,
-        author: 'Martin Fowler',
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Refactoring to patterns',
-        published: 2008,
-        author: 'Joshua Kerievsky',
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'patterns']
-    },
-    {
-        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-        published: 2012,
-        author: 'Sandi Metz',
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'design']
-    },
-    {
-        title: 'Crime and punishment',
-        published: 1866,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'crime']
-    },
-    {
-        title: 'The Demon ',
-        published: 1872,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'revolution']
-    },
-]
+const MONGODB_URI=process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
 
 const typeDefs = gql`
   type Book {
     title: String!
-    published: Int
-    author: String!
+    published: Int!
+    author: Author!
     id: ID!
-    genres: [String]!
+    genres: [String!]!
   }
   type Author {
     name: String!
@@ -102,27 +44,25 @@ const typeDefs = gql`
   type Mutation {
     addBook(
       title: String!
-      published: Int
+      published: Int!
       author: String!
-      genres: [String]!
-    ): Book,
-    editAuthor(
-    name: String!, 
-    setBornTo: Int!
-    ): Author
+      genres: [String!]!
+    ): Book!
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
 const resolvers = {
     Query: {
-        bookCount: () => books.length,
-        authorCount: () => authors.length,
-        allBooks: (root, args) => {
+        bookCount: async () => Book.collection.countDocuments(),
+        authorCount: async () => Author.collection.countDocuments(),
+        allBooks: async (root, args) => {
+            const books = await Book.find({}).populate('author')
             let filteredBooks = [...books]
 
             if (args.author) {
                 filteredBooks = filteredBooks
-                    .filter(book => book.author === args.author)
+                    .filter(book => book.author.name === args.author)
             }
 
             if (args.genre) {
@@ -132,41 +72,34 @@ const resolvers = {
 
             return filteredBooks
         },
-        allAuthors: () => authors
+        allAuthors: async () => Author.find({})
     },
     Author: {
-        bookCount: (root) => {
-            return books
-                .filter(book => book.author === root.name)
-                .length
+        bookCount: async (root) => {
+            const author = await Author.find({ name: root.name })
+            const countedBooks = await Book.countDocuments({ author })
+            return countedBooks
         }
     },
     Mutation: {
-        addBook: (root, args) => {
-            const author = authors.find(author => author.name === args.author)
+        addBook: async (root, args) => {
+            const author = await Author.findOne({ name : args.author })
 
             if (!author) {
-                const newAuthor = {
-                    name: args.author,
-                    id: uuid()
-                }
-                authors = authors.concat(newAuthor)
+                const newAuthor = new Author({ name: args.author })
+                await newAuthor.save()
             }
 
-            const book = {
-                ...args,
-                id: uuid()
-            }
-            books = books.concat(book)
+            const book = new Book({ ...args, author: author._id })
 
-            return book
+            return book.save()
         },
-        editAuthor: (root, args) => {
-            const author = authors.find(author => author.name === args.name)
+        editAuthor: async (root, args) => {
+            const author = await Author.findOne({ name : args.name })
 
             if (author) {
                 author.born = args.setBornTo
-                return author
+                return author.save()
             }
             return null
         }
