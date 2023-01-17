e67be989b0e5234a150a7bf5922bae6e5ccea918
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-01-17 13:36:14 +0300

    9.1 Body mass index

---
+++ part9/exercises 9.1-9.7/bmiCalculator.ts
@@ -0,0 +1,26 @@
+const calculateBmi = (height: number, mass: number): string => {
+    const bmi = mass/((height/100)**2);
+
+    switch(true) {
+        case bmi < 16:
+            return 'Underweight (Severe thinness)';
+        case bmi <= 16.9:
+            return 'Underweight (Moderate thinness)';
+        case bmi <= 18.4:
+            return 'Underweight (Mild thinness)';
+        case bmi <= 24.9:
+            return 'Normal range';
+        case bmi <= 29.9:
+            return 'Overweight (Pre-obese)';
+        case bmi <= 34.9:
+            return 'Obese (Class I)';
+        case bmi <= 39.9:
+            return 'Obese (Class II)';
+        case bmi >= 40:
+            return 'Obese (Class III)';
+        default:
+            return 'Something went wrong.';
+    }
+}
+
+console.log(calculateBmi(180, 74));
