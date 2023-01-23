68858ce6040a1f47d61d7d7943f6a17529366a98
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-01-23 16:10:30 +0300

    9.19: Patientor, step4

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,10 +1,60 @@
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+    'Healthy' = 0,
+    'LowRisk' = 1,
+    'HighRisk' = 2,
+    'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
+
 export type Diagnose = {
     code: string;
     name: string;
     latin?: string;
 }
 
-export interface Entry {}
+export enum Gender {
+    Male = "male",
+    Female = "female",
+    Other = "other"
+}
 
 export interface Patient {
     id: string;
@@ -19,9 +69,3 @@ export interface Patient {
 export type NewPatient = Omit<Patient, 'id'>;
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
-
-export enum Gender {
-    male = "male",
-    female = "female",
-    other = "other"
-}
